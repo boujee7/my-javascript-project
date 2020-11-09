ef3c7f44d984096d82a86d0387621dad4e7ae29e
boujee <akaborgu@gmail.com>
2020-11-09 12:00:50 +0100

    second commit

---
+++ work2 stylejava/myscript.js
@@ -0,0 +1,95 @@
+
+  // alert javascript popup
+  alert("This is my javascript work");
+
+// adding
+var x = 10;
+var y = 4;
+x += y;
+console.log(x)
+//  equal to 
+var x = 10;
+var y = 6;
+x = y;
+console.log(x)
+// subtracting
+var x = 10;
+var y = 2;
+x -= y;
+console.log(x)
+// multiplying
+var x = 10;
+var y = 7;
+x *= y;
+console.log(x)
+// dividing
+var x = 10;
+var y = 3;
+x /= y;
+console.log(x)
+// remainder
+var x = 10;
+var y = 5;
+x %= y;
+console.log(x)
+// equal to 
+var x = 1 == 1
+console.log(x)
+// equal value and equal type 
+var x = 12 === 10
+console.log(x)
+// not equal 
+var x = 12 !=10
+console.log(x)
+// not equal value or not equal type
+var x = 12 !== 10
+console.log(x)
+// greater than
+var x = 12 > 10
+console.log(x)
+// less than
+var x = 12 < 10
+console.log(x)
+// greater than or equal to
+var x = 12 >= 10
+console.log(x)
+// less than or equal to
+var x = 12 <= 10
+console.log(x)
+// area of rightangle triangle
+function area(l,w,b) {
+var Areax =  l * w * b;
+console.log (Areax);    
+}
+area(4,50,6)
+//work done to newton convertion
+function work(f,d) {
+var Workx = f * d;
+console.log (Workx);    
+}
+work(4,50)
+//circumference of a circle
+function circumference( c,pi,r) {
+var circumferencex= 2 * pi * r;
+console.log (circumferencex);    
+}
+circumference(2,3,4)
+//degree to radian convertion
+function degree(d,r) {
+var Degreex = d * 3.14 / 180;
+console.log (Degreex);    
+}
+degree(6)
+
+
+
+
+
+
+      
+
+
+
+
+
+
